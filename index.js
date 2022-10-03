9bf7eb5d8ca53c6b0149b63fb42b8c1add518752
Melody M <melody.masson55@laposte.net>
2022-10-03 15:49:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello world!")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send("hello world!"))
+
+app.listen(port, ()=> console.log("Listening on port " + port))
